a75f5e8af7722347851dcdcacba6c9ac07c85481
Stéphane Loegel <loegel.stephane@gmail.com>
2024-02-29 00:27:00 +0100

    Add GooELog as a submodule

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#define GOOE_LOG_IMPLEMENTATION
+#include <gooeLog/log.h>
 
 int main(int argc, char** argv) {
-    printf("Hello world!");
+    LOG_INFO("For your %s", "INFO");
+    LOG_DEBUG("For your %s", "DEBUG");
+    LOG_WARN("For your %s", "WARNING");
+    LOG_ERR("For your %s", "ERROR");
 }
