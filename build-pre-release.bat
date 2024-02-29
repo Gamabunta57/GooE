a01b18d92309dc4e12da2ca53388275e09e099ee
Stéphane Loegel <loegel.stephane@gmail.com>
2024-02-29 23:21:30 +0100

    Add SDL3 as a submodule
    Update the build procedure in the README file
    And update the main.c to add a quick SDL3 test
    Fix the .gitignore to stop ignoring submodules

--- build-pre-release.bat
+++ build-pre-release.bat
@@ -7,14 +7,17 @@ REM Scripts inspired by Kohi Game Engine
 if not exist .\bin\pre-release MKDIR .\bin\pre-release
 
 SET files=src/*.c
-SET output=bin/pre-release/app.exe
+SET outputDir=bin/pre-release/
+SET output=%outputDir%app.exe
 
 SET flags=-Wall -Werror -std=c99 -O3 -g
 REM -Ipath
-SET includeFlags=-Ilibs/GooELog/src/
+SET includeFlags=-Ilibs/GooELog/src/ -Ivendors/sdl/include/
 REM -lfile
-SET linkerFlags= 
+SET linkerFlags=-Lvendors/build/sdl/lib -lSDL3
 SET defines=-DDEBUG
 
 ECHO "Building application in PRE-RELEASE mode"
 clang %files% %flags% %includeFlags% %linkerFlags% %defines% -o %output%
+
+XCopy "vendors/build/sdl/bin/SDL3.dll" "%outputDir%" /S /F /Y
--- src/main.c
+++ src/main.c
@@ -1,10 +1,30 @@
+#include <process.h>
 #include <stdio.h>
 #define GOOE_LOG_IMPLEMENTATION
 #include <gooeLog/log.h>
+#include <SDL3/SDL.h>
+
+void init();
+void quit();
 
 int main(int argc, char** argv) {
     LOG_INFO("For your %s", "INFO");
     LOG_DEBUG("For your %s", "DEBUG");
     LOG_WARN("For your %s", "WARNING");
     LOG_ERR("For your %s", "ERROR");
+
+    init();
+    quit();
+}
+
+void init() {
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        LOG_ERR("SDL could not initialise");
+        exit(1);
+    }
+    LOG_INFO("SDL initialisation success.");
+}
+
+void quit() {
+    SDL_Quit();
 }
